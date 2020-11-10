9957f681856f704124f6c8cc2d08ec7872802ff3
VAF <kckgvaf@gmail.com>
2020-11-10 20:32:17 +0100

    Mock data, ngIf, ngFor, css class binding

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,20 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero'
+import {HEROES} from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent implements OnInit {
+export class HeroesComponent implements OnInit 
+{
+  //Properties
+  selectedHero: Hero;
 
-  myHero: Hero = {
-    id: 1,
-    name : 'WindStorm'
-  }
+  heroes = HEROES;
+
+  //Methods
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {  }
+
+  onSelect(h : Hero): void
+  {
+    this.selectedHero = h;    
   }
 
 }
